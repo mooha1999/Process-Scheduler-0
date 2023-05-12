f026a9a4c791d3c71c7f9f258878d456c4587757
Nouran <102381294+pseudoengineerr@users.noreply.github.com>
2023-05-12 18:21:43 +0300

    Update process.h

--- Project1/process.h
+++ Project1/process.h
@@ -50,9 +50,8 @@ public:
 	void setTT(int tt);
 	void setTRT(int tt, int at);
 	void setWT(int tt, int at, int ct);
-	void setpair(Queue<pair<int, int>> Q);
+	void addpair(int r, int d);
 
-	void add();
 
 
 
@@ -60,5 +59,5 @@ public:
 	//void getstate();
 
 	void incEX();
-	void add();
+
 };
