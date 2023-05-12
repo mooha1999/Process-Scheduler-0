358f56c3f0fb22a142e3f665381d67f1978520a2
GannaSalah <ganna800874@gmail.com>
2023-05-12 16:52:57 +0300

    ss

--- Project1/RRobin.h
+++ Project1/RRobin.h
@@ -2,12 +2,13 @@
 #include "processor.h"
 #include <string>
 #include"process.h"
+#include"cicularqueue.h"
 //fffff
 class RRobin 
 {
 public:
 
-    Cirularqueue Rdy;
+    Circularqueue Rdy;
     process* RUN;
     int time_slice;
     int Num_of_jobs;
@@ -22,10 +23,11 @@ public:
     else
     {
 
-      RUN=rdy.front(RUN);
-       rdy.pop();
-       //case 1  if the CT is equal to time slice 
-       if (RUN->getCT()== time_slice)
+      RUN=Rdy.front(RUN);
+       Rdy.pop();
+       //case 1  if the execution time is equal to time slice
+
+       if (RUN->getEX()== time_slice)
        {
            rdy.rear(RUN);
        }
