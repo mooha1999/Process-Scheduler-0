b49b04a6aee77a70df5fb5629b8e82689d0a7fb1
Nouran <102381294+pseudoengineerr@users.noreply.github.com>
2023-05-13 22:28:19 +0300

    IMPORTANT

--- Project1/UI.h
+++ Project1/UI.h
@@ -14,6 +14,10 @@ using namespace std;
 
 class UserInterface {
 public:
+    FCFS fcfs;
+    SJF sjf;
+    RRobin rr;
+
     void displayMainMenu() {
         cout << "Process Scheduler Program \n";
         cout << "Please choose from the interface modes: \n";
@@ -31,13 +35,14 @@ public:
     }
     void UserInerface()
     {
-        
 
 
-   // void displayOutput(Queue<int> output) { //displays a queue of intergers
-     //   for (int i : output) {
-          //  cout << output.Pop() << "  ";
-    //}
+
+        // void displayOutput(Queue<int> output) { //displays a queue of intergers
+          //   for (int i : output) {
+               //  cout << output.Pop() << "  ";
+         //}
+    }
 
         void Userchoice() // to get user what mood does the user need 
         {
@@ -65,31 +70,32 @@ public:
 
 
 
-   void displayErrorMessage(string message) {
-        cout << "Error: " << message << "\n";
-    }
+        void displayErrorMessage(string message) {
+            cout << "Error: " << message << "\n";
+        }
 
-    void displaySuccessMessage(string msg) {
-       cout << "Done: " << msg << "\n";
-    }
+        void displaySuccessMessage(string msg) {
+            cout << "Done: " << msg << "\n";
+        }
 
-    Queue<string> readInputFile(string filename) {
-        Queue<string> input;
-        ifstream file(filename);
-        if (file.is_open()) {
-            string line;
-            while (getline(file, line)) 
-            {
-                input.Push(line);
+        Queue<string> readInputFile(string filename) {
+            Queue<string> input;
+            ifstream file(filename);
+            if (file.is_open()) {
+                string line;
+                while (getline(file, line))
+                {
+                    input.Push(line);
+                }
+                file.close();
+                return input;
             }
-            file.close();
-            return input;
+            else {
+                //if file didn't open, show error message 
+                throw runtime_error("Unable to open file");
+            }
         }
-        else {
-            //if file didn't open, show error message 
-            throw runtime_error("Unable to open file");
-        }
-    }
+    
 
     void interfacemode() {
         cout << "Current Timestep:" << "  " << Scheduler.timestep;
