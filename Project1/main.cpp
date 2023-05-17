688cb4264fb794ae0597763233d4ed7f0f7db68f
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2023-05-17 20:28:09 +0300

    main changes

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -13,21 +13,7 @@
 
 using namespace std;
 void main() {
-
-	UserInterface UI;
-	
-	UI.displayMainMenu(); //user chooses a mode 
-	UI.getUserInput(); //now we have user's choice 
-	int x = UI.getUserInput(); //now user's choice assigned to x
-
-	system("cls"); // clear the screen for better UX :) 
-
-	//UI.display(_____); //fill in by inputs for function
-
-	UI.endline(); //line: "PRESS ANY KEY...."
-
-	//functions by scheduler that prints out the output files
-
-
-	
+	Scheduler* scheduler = new Scheduler();
+	scheduler->readFile();
+	scheduler->StartSimulation();
 };
