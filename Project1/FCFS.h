9674c03b39d64fb25a0b63ee7b9998bef8bd082f
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2023-05-17 21:14:01 +0300

    final final

--- Project1/FCFS.h
+++ Project1/FCFS.h
@@ -15,93 +15,88 @@ public:
 	}
 
 	virtual void schedulago(int timestep)
-    {
+	{
 		if (!Rdy->IsEmpty() && !RUN)
 		{
 			RUN = Rdy->Pop();   //return the value of the firt process in rdy list
-			RUN->setRT(timestep - (RUN->getAT())); 
+			RUN->setRT(timestep - (RUN->getAT()));
 			//Response time is difference between current timestep and the arrival time to the processor
 		}
 		else if (RUN)
 		{
 			BUSY = true; //busy when running
 			TBT++; //total busy time
-		}
-
-		else if (RUN->getEX() == RUN->getCT())
-				{
-					RUN->setTT(timestep); //now the termination time of process is equal to current timestep
-					AR = AR + RUN->getTRT(); // total turn around time of all processes
-					Finish = RUN;
-					RUN = nullptr;
-					BUSY = false;
-				}
-				else if (!RUN->getPairs().IsEmpty() && RUN->getEX() == RUN->getPairs().Peek()->first)
-				{
-					Blk = RUN;
-					RUN = nullptr;
-				}
-				else
-				{
-					RUN->incEX();
-				}
+			if (RUN->getEX() == RUN->getCT())
+			{
+				RUN->setTT(timestep); //now the termination time of process is equal to current timestep
+				AR = AR + RUN->getTRT(); // total turn around time of all processes
+				Finish = RUN;
+				RUN = nullptr;
+				BUSY = false;
 			}
-	virtual Queue<int> GetID()
-	{
-		Queue<Process*>temp = *Rdy;   //*Rdy to return the value of Rdy (copy)
-		Queue<int>ID;
-		while (temp.IsEmpty())
-		{
-			int x = temp.Pop()->getPID();  //return id
-			ID.Push(x); //push the id in the queue
-		}
-		return ID;
-	}
-	virtual int GetTWT()
-	{
-		Queue<Process*>temp = *Rdy;
-		int SumWT = 0;
-		while (!temp.IsEmpty())
-		{
-			int x = temp.Pop()->getWT();  //return waiting time
-			SumWT = SumWT + x;
-		}
-		return SumWT;
-	}
-
-	Process* kill(int id) {
-		//for running processes
-		int z = RUN->getPID();  //return id of running process
-		if (z == id) {
-			Process* killPR = RUN;
-			RUN = nullptr;
-			BUSY = false;
-			return killPR;
-		}
-		//for rdy processes
-		for (Process* i : *Rdy) {
-			int y = i->getPID();  //return id from ready queue
-			if (y == id) {
-				Process* killPD = i;
-				//removing the process
-				Rdy->Remove(i);
-				return killPD;
+			else if (!RUN->getPairs().IsEmpty() && RUN->getEX() == RUN->getPairs().Peek()->first)
+			{
+				Blk = RUN;
+				RUN = nullptr;
+			}
+			else
+			{
+				RUN->incEX();
 			}
 		}
+		virtual Queue<int> GetID()
+		{
+			Queue<Process*>temp = *Rdy;   //*Rdy to return the value of Rdy (copy)
+			Queue<int>ID;
+			while (temp.IsEmpty())
+			{
+				int x = temp.Pop()->getPID();  //return id
+				ID.Push(x); //push the id in the queue
+			}
+			return ID;
+		}
+		virtual int GetTWT()
+		{
+			Queue<Process*>temp = *Rdy;
+			int SumWT = 0;
+			while (!temp.IsEmpty())
+			{
+				int x = temp.Pop()->getWT();  //return waiting time
+				SumWT = SumWT + x;
+			}
+			return SumWT;
+		}
 
-		return nullptr;
-	}
-	virtual int GetCount()
-	{
-		return Rdy->Count();
-	}
+		Process* kill(int id) {
+			//for running processes
+			int z = RUN->getPID();  //return id of running process
+			if (z == id) {
+				Process* killPR = RUN;
+				RUN = nullptr;
+				BUSY = false;
+				return killPR;
+			}
+			//for rdy processes
+			for (Process* i : *Rdy) {
+				int y = i->getPID();  //return id from ready queue
+				if (y == id) {
+					Process* killPD = i;
+					//removing the process
+					Rdy->Remove(i);
+					return killPD;
+				}
+			}
 
-	virtual int Getidrun() //return the id of the running process 
-	{
-		int r = RUN->getPID();
-		return r;
-	}
+			return nullptr;
+		}
+		virtual int GetCount()
+		{
+			return Rdy->Count();
+		}
 
-
-
-};
+		virtual int Getidrun() //return the id of the running process
+		{
+			int r = RUN->getPID();
+			return r;
+		}
+	};
--- Project1/SJF.h
+++ Project1/SJF.h
@@ -1,7 +1,7 @@
 #include "processor.h"
 #include"process.h"
 #include"PriortyQueue.h"
-#pragma once 
+#pragma once
 
 class SJF :public Processor
 {
@@ -16,7 +16,7 @@ public:
 
 	virtual void schedulago(int timestep)
 	{
-		if (!Rdy->IsEmpty() && !RUN) //check if rdy queue isn't empty 
+		if (!Rdy->IsEmpty() && !RUN) //check if rdy queue isn't empty
 		{
 			RUN = Rdy->Pop();   //return the value of the firt process in rdy list
 			RUN->setRT(timestep - (RUN->getAT()));
@@ -26,59 +26,55 @@ public:
 		{
 			BUSY = true; //busy when running
 			TBT++; //total busy time
-		}
 
-			else if (RUN->getEX() == RUN->getCT())
-				{
-					RUN->setTT(timestep);
-					AR = AR + RUN->getTRT();
-					Finish = RUN;
-					RUN = nullptr;
-				}
-			else if (!RUN->getPairs().IsEmpty() && RUN->getEX() == RUN->getPairs().Peek()->first)
-				{
-					Blk = RUN;
-					RUN = nullptr;
-				}
-			else
-				{
-					RUN->incEX();
-				}
+			if (RUN->getEX() == RUN->getCT())
+			{
+				RUN->setTT(timestep);
+				AR = AR + RUN->getTRT();
+				Finish = RUN;
+				RUN = nullptr;
 			}
-
-
-virtual Queue<int> GetID()
-	{
-		PriortyQueue<Process*>temp = *Rdy;
-		Queue<int>ID;
-		while (temp.IsEmpty())
-		{
-			int x = temp.Pop()->getPID();  //return id
-			ID.Push(x);
+			else if (!RUN->getPairs().IsEmpty() && RUN->getEX() == RUN->getPairs().Peek()->first)
+			{
+				Blk = RUN;
+				RUN = nullptr;
+			}
+			else
+			{
+				RUN->incEX();
+			}
 		}
-		return ID;
-	}
-	virtual int GetTWT()
-	{
-		PriortyQueue<Process*>temp = *Rdy;
-		int SumWT = 0;
-		while (!temp.IsEmpty())
+
+		virtual Queue<int> GetID()
 		{
-			int x = temp.Pop()->getWT();  //return waiting time
-			SumWT = SumWT + x;
+			PriortyQueue<Process*>temp = *Rdy;
+			Queue<int>ID;
+			while (temp.IsEmpty())
+			{
+				int x = temp.Pop()->getPID();  //return id
+				ID.Push(x);
+			}
+			return ID;
 		}
-		return SumWT;
-	}
-	virtual int GetCount()
-	{
-		return Rdy->Count();
-	}
+		virtual int GetTWT()
+		{
+			PriortyQueue<Process*>temp = *Rdy;
+			int SumWT = 0;
+			while (!temp.IsEmpty())
+			{
+				int x = temp.Pop()->getWT();  //return waiting time
+				SumWT = SumWT + x;
+			}
+			return SumWT;
+		}
+		virtual int GetCount()
+		{
+			return Rdy->Count();
+		}
 
-	virtual int Getidrun()  //return the id of the running process 
-	{
-		int r = RUN->getPID();
-		return r;
-	}
-
-
-};
+		virtual int Getidrun()  //return the id of the running process
+		{
+			int r = RUN->getPID();
+			return r;
+		}
+	};
